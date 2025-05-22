33149634629bde173a8b18e11e17eda20ffed65f
Digi <matteo.digifico@itiscuneo.edu.it>
2025-05-22 11:32:34 +0200

    Aggiungere i file di progetto.

---
+++ PlayVault/Models/Game.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlayVault.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        public string Image {  get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(500, MinimumLength = 5)]
+        [Required]
+        public string Description { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 150)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+        [Range(0, 110)]
+        [Required]
+        public int Rating { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(500, MinimumLength = 5)]
+        [Required]
+        public string recensioneTxt { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Piattaforma { get; set; }
+
+
+    }
+
+}
