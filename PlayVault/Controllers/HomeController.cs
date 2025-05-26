72a045da9309db9bbe9284694626ec2f39390a03
Tommaso Spada <124589023+TommySpadaITIS@users.noreply.github.com>
2025-05-26 15:47:50 +0200

    added Ufficio.cshtml, edited controllers, added migrations for utentes db table

--- PlayVault/Controllers/HomeController.cs
+++ PlayVault/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace PlayVault.Controllers
             return View();
         }
 
+        public IActionResult Ufficio()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
